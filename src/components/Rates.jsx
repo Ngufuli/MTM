746d2e3009b12c831c3c4e90329263c9f4a902d1
Ngufuli <ngufuli@gmail.com>
2024-03-10 05:08:54 +0530

    .

---
+++ src/components/Rates.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rates = () => {
+  return (
+    <div>
+      <h2>Exchange Rates</h2>
+      {/* Add exchange rate data here */}
+    </div>
+  );
+};
+
+export default Rates;
