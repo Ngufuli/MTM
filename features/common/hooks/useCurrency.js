d5beb2e3d9188bb653328e029d379ae15575ee82
Ngufuli <ngufuli@gmail.com>
2024-03-25 04:15:23 +0530

    date,time,currencies,conversion, rate and symbols

--- features/common/hooks/useCurrency.js
+++ features/common/hooks/useCurrency.js
@@ -11,14 +11,44 @@ export const useCurrency  = () => {
     {queries:[{
         queryKey: ["rates", currencyOne],
         queryFn: () => fetchRates(currencyOne),
-        staleTime: Infinity
+        staleTime: Infinity,
+        select: ({rates, date, timestamp}) => {
+            return {rates, date, timestamp}
+        }
     },
     {
         queryKey: ["symbols"],
         queryFn: () => fetchSymbols,
-        staleTime: Infinity  
+        staleTime: Infinity,
+        select: ({symbols}) => symbols
     }]}
     ) 
 
-    return {amount, currencyOne, currencyTwo}
+    const isLoading = [ratesData, symbolsData].some((query) => query.isLoading);
+    const isError = [ratesData, symbolsData].some((query) => query.isError);
+
+    const convertedAmount = (ratesData.data?.rates[currencyTwo] * amount).toFixed(2)
+
+    const date = new Date(ratesData.data?.date).toLocaleDateString();
+    const time = new Date(ratesData.data?.timestamp).toLocaleTimeString('en-US');
+
+    const currencyList = symbolsData.data ? Object.keys(symbolsData.data) : {};
+
+    console.log(ratesData)
+    return {
+        isLoading,
+        isError,
+        amount, 
+        setAmount,
+        currencyOne, 
+        setCurrencyOne,
+        currencyTwo,
+        setCurrencyTwo,
+        ratesData,
+        symbolsData,
+        convertedAmount,
+        date,
+        time,
+        currencyList
+    }
 }
