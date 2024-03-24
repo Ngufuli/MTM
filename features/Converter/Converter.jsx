c48403867b0e7aaed48db2aece9dc1eca7a4fb96
Ngufuli <ngufuli@gmail.com>
2024-03-25 04:25:14 +0530

    import and chakra

--- features/Converter/Converter.jsx
+++ features/Converter/Converter.jsx
@@ -1,7 +1,36 @@
+import { Spinner } from "@chakra-ui/spinner";
+import { Text } from "@chakra-ui/layout";
 import { useCurrency } from "../common/hooks/useCurrency";
 
 const Converter = () => {
-    const {amount, currencyOne, currencyTwo} = useCurrency()
+    const {
+        isLoading,
+        isError,
+        amount, 
+        setAmount,
+        currencyOne, 
+        setCurrencyOne,
+        currencyTwo,
+        setCurrencyTwo,
+        ratesData,
+        symbolsData,
+        convertedAmount,
+        date,
+        time,
+        currencyList
+    } = useCurrency();
+
+    if(isError){
+        return(
+            <Text fontWeight='bold' fontSize='3xl' color='white' my='10'>Something has gone wrong</Text>
+        )
+    }
+
+    if(isLoading){
+        return(
+            <Spinner margin='auto 0' size='xl' thickness="4px" speed="0.66s" color="purple.500" emptyColor="purple.200"></Spinner>
+        )
+    }
     return(
         <>
             <>{amount}</>
