c3ef037d9277bb7cbc8299ce105def86040eba8f
Ngufuli <ngufuli@gmail.com>
2024-04-05 14:51:35 +0530

    revamp

--- features/common/hooks/useFlags.js
+++
@@ -1,5 +0,0 @@
-export  const useFlags = (flags) => {
-    const flagUrl = `https://wise.com/public-resources/assets/flags/rectangle/${flags.toLowerCase()}.png`
-
-    return {flagUrl};
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ChakraProvider } from '@chakra-ui/react'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-const queryClient = new QueryClient()
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <App />
-        <ReactQueryDevtools ></ReactQueryDevtools>
-      </ChakraProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 )
